249152331b16e4687b1c06f5d921e022431fb57e
Ivan Gauto <ivangauto@hotmail.com>
2020-07-21 14:10:20 -0300

    Primer Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Aplicacion de comandos
+
+Ejercicio del curso
+
+Instalar paquetes de node
+
+npm install
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,93 @@
+/* jshint esversion: 6 */
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDb = () => {
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se puedo grabar', err);
+    });
+};
+
+
+
+
+const cargarDb = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+};
+
+
+
+const crear = (descripcion) => {
+    cargarDb();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDb();
+
+    return porHacer;
+
+};
+
+const getListado = () => {
+    cargarDb();
+    return listadoPorHacer;
+};
+
+const actualizar = (descripcion, completado = true) => {
+    cargarDb();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDb();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const borrar = (descripcion) => {
+    cargarDb();
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+    if (index >= 0) {
+        listadoPorHacer.splice(index, 1);
+        guardarDb();
+        return true;
+    } else {
+        return false;
+    }
+
+
+    // usando filter
+    /* let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion;
+    });
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDb();
+        return true;
+    }  */
+};
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+};
